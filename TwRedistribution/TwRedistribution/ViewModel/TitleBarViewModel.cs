e0b952339e23df3c02833b797175124d26c7a4dd
이성현 <dljdgus1211@naver.com>
2020-03-07 17:07:55 +0900

    Added Title bar viewmodel.

---
+++ TwRedistribution/TwRedistribution/ViewModel/TitleBarViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace TwRedistribution.ViewModel
+{
+    class TitleBarViewModel : ViewModelBase
+    {
+        #region Command
+        public RelayCommand<object> ButtonClickCommand { get; private set; }
+
+        private void InitRelayCommand()
+        {
+            ButtonClickCommand = new RelayCommand<object>((param) => OnButtonClick(param));
+        }
+
+        private void OnButtonClick(object param)
+        {
+            switch (param.ToString())
+            {
+                case "Exit":
+                    ExitProgramClick();
+                    break;
+            }
+        }
+
+        public void ExitProgramClick()
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+        #endregion
+
+        public TitleBarViewModel()
+        {
+            InitRelayCommand();
+        }
+    }
+}
