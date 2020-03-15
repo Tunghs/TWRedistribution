2ac8521374d78f9cd59ccdd0cb75a0213b553459
이성현 <dljdgus1211@naver.com>
2020-03-15 23:04:17 +0900

    Added listview item selected command

--- TwRedistribution/TwRedistribution/ViewModel/ListViewModel.cs
+++ TwRedistribution/TwRedistribution/ViewModel/ListViewModel.cs
@@ -1,10 +1,46 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows;
 
 namespace TwRedistribution.ViewModel
 {
     public class ListViewModel : ViewModelBase
     {
+        #region UI Variable
+        private int _listViewIndex = 0;
+        public int ListViewIndex
+        {
+            get { return _listViewIndex; }
+            set { _listViewIndex = value; RaisePropertyChanged("ListViewIndex"); }
+        }
+        #endregion
 
+        #region Command
+        public RelayCommand<EventArgs> SelectedListViewItemCommand { get; private set; }
+
+        private void InitRelayCommand()
+        {
+            SelectedListViewItemCommand = new RelayCommand<EventArgs>(OnSelectedListViewItemIndex);
+        }
+
+        private void OnSelectedListViewItemIndex(EventArgs e)
+        {
+            switch (ListViewIndex)
+            {
+                case 0:
+                    MessageBox.Show("0번");
+                    break;
+                case 1:
+                    MessageBox.Show("1번");
+                    break;
+            }
+        }
+        #endregion
+
+        public ListViewModel()
+        {
+            InitRelayCommand();
+        }
     }
 }
