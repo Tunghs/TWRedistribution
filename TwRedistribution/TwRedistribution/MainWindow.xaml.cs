1aef2c971f2e0cbb028a9b58a89a6d71b9a29eb3
이성현 <dljdgus1211@naver.com>
2020-03-22 21:50:22 +0900

    Apply theme

--- TwRedistribution/TwRedistribution/MainWindow.xaml.cs
+++ TwRedistribution/TwRedistribution/MainWindow.xaml.cs
@@ -13,12 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MahApps.Metro.Controls;
+
 namespace TwRedistribution
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
