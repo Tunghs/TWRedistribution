1ee37e8653c3e4d854abb5cfb40c48966dad9d39
이성현 <dljdgus1211@naver.com>
2020-03-22 18:41:21 +0900

    Changed UI

--- TwRedistribution/TwRedistribution/Properties/Resources.Designer.cs
+++ TwRedistribution/TwRedistribution/Properties/Resources.Designer.cs
@@ -81,12 +81,32 @@ namespace TwRedistribution.Properties {
         }
         
         /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo {
+            get {
+                object obj = ResourceManager.GetObject("Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   (아이콘)과(와) 유사한 System.Drawing.Icon 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
         internal static System.Drawing.Icon Minimize {
             get {
                 object obj = ResourceManager.GetObject("Minimize", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap Nocturne {
+            get {
+                object obj = ResourceManager.GetObject("Nocturne", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
