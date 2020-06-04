83bcc0986ca250142245903d68630e31da46be39
이성현 <dljdgus1211@naver.com>
2020-06-04 23:54:54 +0900

    add StatusController

--- TwRedistribution/TW_Controls/Controls/StatusController.cs
+++ TwRedistribution/TW_Controls/Controls/StatusController.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace TW_Controls.Controls
 {
-    class StatControl : Control
+    class StatusController
     {
     }
 }
